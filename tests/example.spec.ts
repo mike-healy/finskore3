e1e302e84dea5e2e5273c25fddf81cdff7791db7
Mike Healy <design@mikehealy.com.au>
2026-04-09 08:02:48 +1000

    Install Playwright

---
+++ tests/example.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('/');
+
+  await expect(page).toHaveTitle(/Finskore/);
+});
+
+// Skipped example test…
+test.skip('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
